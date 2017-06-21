827732f3c623397b6381b4ba5dc53192172c1daf
maozuowei <540329033@qq.com>
2017-06-21 13:49:09 +0800

    printnt_lol

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core  import setup
+
+setup(
+        name           = 'nester',
+        version        = '1.1.0',
+        py_modules     = ['nester'],
+        author         = 'mao',
+        author_email   = '540329033@qq.com',
+        url            =
+        description    = 'A simple printer of nested lists',
+    )
